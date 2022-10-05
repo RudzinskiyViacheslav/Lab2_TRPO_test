4f9d8b4be1d3295a26f0356897f13566a872d96d
Egor Zhidkov <e.jidkov@gmail.com>
2022-10-05 14:35:21 +0300

    Git commit by EgorZhidkov

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include "askname.h"
 int main(int argc, char **argv)
 {
-char first[255], last[255];
-askname(first, last);
-printf("Hello, %s %s!\n", first, last);
-return 0;
+    char first[255], last[255];
+    askname(first, last);
+    printf("Hello, %s %s!\n", first, last);
+    printf("Hello, I'm Sasukee!!!");
+    return 0;
 }
